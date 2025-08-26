41adb75d14dd1971e59c7a314df758a4f60d1cc2
alaa <alaa@example.com>
2025-08-26 15:36:56 +0300

    add final Modify

--- variables.tf
+++ variables.tf
@@ -1,2 +1,10 @@
-variable "cloud_api_2" {}
-variable "c_account" {}
+variable "cloud_api_2" {
+  description = "Cloudflare API Token"
+  type        = string
+  sensitive   = true
+}
+
+variable "c_account" {
+  description = "Cloudflare Account ID"
+  type        = string
+}
