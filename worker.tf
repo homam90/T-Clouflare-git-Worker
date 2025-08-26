9409a21e215568d4427c54e84491cffb731ce2ca
alaa <alaa@example.com>
2025-08-26 14:41:47 +0300

    add final step

--- worker.tf
+++ worker.tf
@@ -1,6 +1,16 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
 provider "cloudflare" {
   api_token = var.cloudflare_api_2
 }
+
 
 resource "cloudflare_worker_script" "remoote_worker" {
   name    = "my-remoote_worker"               # Name of the new Worker
