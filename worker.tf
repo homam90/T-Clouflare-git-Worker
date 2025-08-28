4b6c06e967213ef89796a4651bacc93f5d7033d5
alaa <alaa@example.com>
2025-08-28 14:07:47 +0300

    add final Modify

--- worker.tf
+++ worker.tf
@@ -1,20 +1,24 @@
 terraform {
   required_providers {
     cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "~> 4.0"
+      source = "cloudflare/cloudflare"
+      version = "~> 5"
     }
   }
 }
-
 provider "cloudflare" {
   api_token = var.cloud_api_2
  
 }
-
-
-resource "cloudflare_worker_script" "remoote_worker" {
-  name    = "my-remoote_worker"    # Name of the new Worker
-  account_id = var.c_account           
-  content = file("${path.module}/src/index.js")
+resource "cloudflare_workers_script" "my-hello-world-script" {
+  account_id = var.c_account 
+  script_name = "my-hello-world-script"
+  main_module = "my-hello-world-script.mjs"
+  content = trimspace(file("my-hello-world-script.mjs"))
+  compatibility_date = "$today"
+  bindings = [{
+    name = "MESSAGE"
+    type = "plain_text"
+    text = "Hello World!"
+  }]
 }
